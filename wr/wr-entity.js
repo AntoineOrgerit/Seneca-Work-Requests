1690ae23fc6328f5512ec006a64d1d951290d1c5
Antoine Orgerit <orgerit.antoine@gmail.com>
2019-04-15 11:38:10 +0200

    Adding wr-entity storage functions.

---
+++ wr/wr-entity.js
@@ -0,0 +1,80 @@
+var seneca = require('seneca')();
+var entities = require('seneca-entity');
+
+seneca.use(entities);
+var wr_entity = seneca.make$('wr_entity');
+
+exports.create = function(entity) {
+	entity.state = 'created';
+	wr_entity.save$(entity, function(err, result){
+		if(err) {
+			return err;
+		} else {
+			return result;
+		}
+	});
+};
+
+exports.get = function(id) {
+	if(typeof id !== 'undefined'){
+		wr_entity.list$({id: id}, function(err, entity){
+			if(err) {
+				return err;
+			} else {
+				return entity;
+			}
+		});
+	} else {
+		wr_entity.list$(function(err, entities) {
+			if(err) {
+				return err;
+			} else {
+				return entities;
+			}
+		});
+	}
+};
+
+exports.update = function(id, fields) {
+	wr_entity.load$(id, function(err, existing_entity) {
+		if(err) {
+			return 'wr not found';
+		} else {
+			if(typeof existing_entity.state !== 'undefined' && existing_entity.state === 'closed') {
+				return 'wr is already closed';
+			} else {
+				for(var key in fields){
+					existing_entity[key] = fields[key];
+				}
+				wr_entity.save$(existing_entity, function(err, result){
+					if(err) {
+						return err;
+					} else {
+						return result;
+					}
+				});
+			}
+		}
+	});
+};
+
+exports.delete = function(id) {
+	wr_entity.load$(id, function(err, existing_entity) {
+		if(err) {
+			return 'wr not found';
+		} else {
+			if(typeof existing_entity.state !== 'undefined' && existing_entity.state === 'closed') {
+				return 'wr is already closed';
+			} else {
+				wr_entity.remove$(id, function(err){
+					if(err) {
+						return err;
+					} else {
+						return null;
+					}
+				});
+			}
+		}
+	});
+};
+
