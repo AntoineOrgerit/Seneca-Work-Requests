07e541901caac4f23c93b02dff7941972de145b2
AstroChibi <astrochibi@gmail.com>
2019-04-15 14:41:22 +0200

    Adding handling of Retreive, Update and Delete in wr-service

--- wr/wr-service.js
+++ wr/wr-service.js
@@ -7,11 +7,16 @@ var jsonic = require('jsonic');
 
 //Create
 seneca.add('role:wr, cmd:create', function(receivedMsg, respond) {
+
 	let entity = {};
+
 	entity.applicant = receivedMsg.applicant;
 	entity.work = receivedMsg.work;
+
 	wr_entity.create(entity, function(result) {
+
 		let response = {};
+
 		if ("entity$" in result) {
 			response.success = true;
 			response.msg = '';
@@ -22,20 +27,73 @@ seneca.add('role:wr, cmd:create', function(receivedMsg, respond) {
 			response.data = '';
 		}
 		respond(null, response);
+
 	});
 });
 
 //Retrieve
 seneca.add('role:wr, cmd:retrieve', function(msg, respond) {
-  
+
+	wr_entity.get(msg.id, function(result) {
+
+		let response = {};
+
+		if ("entity$" in result) {
+			response.success = true;
+			response.msg = '';
+			response.data = result;
+		} else {
+			response.success = false;
+			response.msg = result
+			response.data = '';
+		}
+
+		respond(null, response);
+
+	});  
 });
 
 //Update
 seneca.add('role:wr, cmd:update', function(msg, respond) {
-  
+	
+	wr_entity.update(msg.id, function(result) {
+
+		let response = {};
+
+		if ("entity$" in result) {
+			response.success = true;
+			response.msg = '';
+			response.data = result;
+		} else {
+			response.success = false;
+			response.msg = result
+			response.data = '';
+		}
+
+		respond(null, response);
+
+	});
+
 });
 
 //Delete
 seneca.add('role:wr, cmd:delete', function(msg, respond) {
-  
-});
+	
+	wr_entity.delete(msg.id, function(result) {
+
+		let response = {};
+
+		if(result == null){
+			response.success = true;
+			response.msg = ''
+			response.data = ''
+		} else {
+			response.success = false;
+			response.msg = result;
+			response.data = ''
+		}
+
+		respond(null, response);
+
+	});
+}); 
