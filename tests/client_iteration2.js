b58cea6b64dad12c33bf51978e85bf92145766b1
AstroChibi <astrochibi@gmail.com>
2019-04-16 10:55:47 +0200

    Adding 2 more tests to ensure correct Update functionment

--- tests/client_iteration2.js
+++ tests/client_iteration2.js
@@ -133,6 +133,20 @@ lab.experiment('Work Request micro-service', () => {
         expect(result.msg).to.be.equals('wr path not supported');
     });
 
+    // testing update with prohibited fields
+    lab.test('Trying to update pierre wr with a prohibited field', async () => {
+        const result = await makePromiseRequest(client.put, '/api/wr/' + pierreWR.id, {"foo": "bar"});
+        expect(result.success).to.be.false();
+        expect(result.msg).to.be.equals('invalid parameter');
+    });
+
+    // testing update with prohibited state value
+    lab.test('Trying to update pierre wr state with a prohibited value', async () => {
+        const result = await makePromiseRequest(client.put, '/api/wr/' + paulWR.id, {"state": "bar"});
+        expect(result.success).to.be.false();
+        expect(result.msg).to.be.equals('invalid value for parameter state (can only be closed)');
+    });
+
 	// testing delete with wrong id
     lab.test('Trying to delete with a dummy id', async () => {
         const result = await makePromiseRequest(client.del, '/api/wr/_______');
